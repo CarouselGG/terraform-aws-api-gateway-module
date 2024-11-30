d5e397bef570901a58226179501142745648a6d9
Loren M. Kerr <loren@carousel.gg>
2024-11-30 08:16:59 -0700

    adding a couple new metrics

--- cloudwatch-dashboard.tf
+++ cloudwatch-dashboard.tf
@@ -27,6 +27,47 @@ resource "aws_cloudwatch_dashboard" "api_gateway_dashboard" {
           "period" : 300,
           "title" : "API Gateway Requests and Errors"
         }
+      },
+
+      // Metric showing current average latency with p99 in single value view
+      {
+        "type" : "metric",
+        "x" : 0,
+        "y" : 6,
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "metrics" : [
+            ["API-Gateway-Metrics", "Latency"],
+            [".", aws_cloudwatch_log_metric_filter.latency_p99.name]
+          ],
+          "view" : "singleValue",
+          "stacked" : false,
+          "region" : "${var.aws_region}",
+          "stat" : "Average",
+          "period" : 300,
+          "title" : "Latency"
+        }
+      },
+      // Success Rate metric that uses a guage view to compare 2xx to 5xx errors. should be green for 2xx and red for 5xx
+      {
+        "type" : "metric",
+        "x" : 12,
+        "y" : 6,
+        "width" : 12,
+        "height" : 6,
+        "properties" : {
+          "metrics" : [
+            [".", aws_cloudwatch_log_metric_filter.errors_5xx.name],
+            [".", aws_cloudwatch_log_metric_filter.errors_2xx.name]
+          ],
+          "view" : "gauge",
+          "stacked" : false,
+          "region" : "${var.aws_region}",
+          "stat" : "Average",
+          "period" : 300,
+          "title" : "Success Rate"
+        }
       }
     ]
   })
