607ba48fd4b6b2950885adf69449d12c9d4296f2
Loren M. Kerr <loren@carousel.gg>
2024-12-01 08:14:31 -0700

    statement id for permissions is now just a uuid. uuid stays same for each route

--- apigateway.tf
+++ apigateway.tf
@@ -41,10 +41,11 @@ resource "aws_apigatewayv2_stage" "api_stage" {
 resource "aws_apigatewayv2_integration" "integrations" {
   for_each = var.routes
 
-  api_id             = aws_apigatewayv2_api.api.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = each.value.invoke_arn
-  integration_method = "POST"
+  api_id                 = aws_apigatewayv2_api.api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = each.value.invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
 }
 
 # Routes
@@ -67,7 +68,7 @@ resource "aws_apigatewayv2_api_mapping" "api_mapping" {
 resource "aws_lambda_permission" "apigw_lambda_permissions" {
   for_each = var.routes
 
-  statement_id  = "AllowExecution-${substr(base64sha256(each.key), 0, 8)}"
+  statement_id  = "AllowExecution-${uuidv5("namespace", each.key)}"
   action        = "lambda:InvokeFunction"
   function_name = each.value.function_name # Access function_name from the Lambda resource
   principal     = "apigateway.amazonaws.com"
